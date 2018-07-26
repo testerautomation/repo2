63f0dc82c454f6a403f96da1b03fd26d6e19565b
test study <studytest66@gmail.com>
2018-07-26 23:48:38 +1000

    first commit

---
+++ artifcat/src/test/java/com/artifcat/AppTest.java
@@ -0,0 +1,28 @@
+package com.artifcat;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+	public void testApp()
+    {
+        AssertJUnit.assertTrue( true );
+    }
+}
